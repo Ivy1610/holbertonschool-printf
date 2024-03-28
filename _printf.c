0b69c906b69766aabe9c7e806c5a4048b522edc2
fatmaGma <8891@holbertonstudents.com>
2024-03-28 11:02:24 +0100

    petit update du code (switch not allowed)

--- _printf.c
+++ _printf.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 int _printf(const char *format, ...) {
-	// declaration et initialisation
+	// déclaration et initialisation
 	va_list args;
 	int count = 0;
 	const char *ptr;
@@ -14,42 +14,46 @@ int _printf(const char *format, ...) {
 		if (*ptr == '%')
 		{
 			ptr++;
-			switch (*ptr)
+			if (*ptr == 'c')
 			{
-				case 'c':
-					count += print_char(va_arg(args, int));
-					break;
-				case 's':
-					count += print_string(va_arg(args, char *));
-					break;
-				case 'd':
-				case 'i':
-					count += print_integer(va_arg(args, int));
-					break;
-				case 'u':
-					count += print_unsigned_int(va_arg(args, unsigned int));
-					break;
-				case 'o':
-					count += print_octal(va_arg(args, unsigned int));
-					break;
-				case 'x':
-				case 'X':
-					count += print_hexadecimal(va_arg(args, unsigned int));
-					break;
-				/** 
-				 * case 'p':
-					count += print_pointer(va_arg(args, void *));
-					break;
-					*/
-				case '%':
-					putchar('%');
-					count++;
-					break;
-				default:
-					putchar('%');
-					putchar(*ptr);
-					count += 2;
-					break;
+				count += print_char(va_arg(args, int));
+			}
+			else if (*ptr == 's')
+			{
+				count += print_string(va_arg(args, char *));
+			}
+			else if (*ptr == 'd' || *ptr == 'i')
+			{
+				count += print_integer(va_arg(args, int));
+			}
+			else if (*ptr == 'u')
+			{
+				count += print_unsigned_int(va_arg(args, unsigned int));
+			}
+			else if (*ptr == 'o')
+			{
+				count += print_octal(va_arg(args, unsigned int));
+			}
+			else if (*ptr == 'x' || *ptr == 'X')
+			{
+				count += print_hexadecimal(va_arg(args, unsigned int));
+			}
+			/** 
+			 * else if (*ptr == 'p')
+			 * {
+			 *     count += print_pointer(va_arg(args, void *));
+			 * }
+			 */
+			else if (*ptr == '%')
+			{
+				putchar('%');
+				count++;
+			}
+			else
+			{
+				putchar('%');
+				putchar(*ptr);
+				count += 2;
 			}
 		}
 		else
@@ -61,3 +65,4 @@ int _printf(const char *format, ...) {
 	va_end(args);
 	return count;
 }
+
